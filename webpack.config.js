d3c5e23d00a20df3a8887db3f8ba08462c0f9c94
Anefu Favour <favouranefu@gmail.com>
2020-07-11 08:16:05 +0100

    Added basic webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/template.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
